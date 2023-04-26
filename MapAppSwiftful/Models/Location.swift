1d0f5d9990d003fff2e84408b24d742d22c6558d
Baris Karalar <b.karalar@jemix.de>
2023-04-26 09:57:38 +0200

    Project Setup with Assets and Custom Data Model | SwiftUI Map App #1 https://www.youtube.com/watch?v=11iFAWFO0AU&list=PLwvDm4Vfkdpha5eVTjLM0eRlJ7-yDDwBk

---
+++ MapAppSwiftful/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  MapAppSwiftful
+//
+//  Created by Baris Karalar on 26.04.23.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
