e886cd5e7a11128fa61111e7d8046ada12033ed2
Baris Karalar <b.karalar@jemix.de>
2023-04-26 09:35:36 +0200

    Initial Commit

---
+++ MapAppSwiftful/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MapAppSwiftful
+//
+//  Created by Baris Karalar on 26.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapAppSwiftful/MapAppSwiftfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppSwiftfulApp.swift
+//  MapAppSwiftful
+//
+//  Created by Baris Karalar on 26.04.23.
+//
+
+import SwiftUI
+
+@main
+struct MapAppSwiftfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
